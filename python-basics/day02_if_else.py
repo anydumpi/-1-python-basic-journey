3285f97862bd042e6ea071e4c4ab6eae12f7fb58
anydumpi <divahelmi.n.a@gmail.com>
2026-02-26 15:10:47 +0700

    Update python-basics

--- python-basics/day02_if_else.py
+++ python-basics/day02_if_else.py
@@ -4,3 +4,10 @@ umur = 20
 if umur < 13: print ("Anak-anak")
 elif umur <= 17: print ("Remaja")
 else: print ("Dewasa")
+
+#===================================
+umur = int (input ("Masukkan umur anda: "))
+membership = input ("Punya membership? (Ya/Tidak): ") .strip() .lower()
+if umur >= 18 and membership == "Ya": print ("Akses penuh")
+elif umur >= 18 and membership == "Tidak": print ("Akses terbatas")
+else: print ("Tidak diizinkan")
---
+++ python-basics/day03_loop.py
@@ -0,0 +1,16 @@
+
+# Challenge 1: Print angka 1-10
+for i in range(1, 11):
+    print(i)
+
+#Challenge 2: Print hanya angka genap dari 1-10
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(i)
+
+#Challenge 3: Minta user input angka, lalu print angka itu sebanyak yang dia masukkan
+angka= int (input ("Input jumlah repeat: "))
+for i in range (angka): print ("Halo")
+
+#Challenge 4: Hitung total dan rata-rata
+angka= int (input ("Input jumlah repeat: "))
