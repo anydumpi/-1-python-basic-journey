fcfff5aeb72b69ae5451777a772df7dbd38d3a78
anydumpi <divahelmi.n.a@gmail.com>
2026-03-01 13:24:48 +0700

    Update

--- python-basics/day03_loop.py
+++ python-basics/day03_loop.py
@@ -13,4 +13,45 @@ angka= int (input ("Input jumlah repeat: "))
 for i in range (angka): print ("Halo")
 
 #Challenge 4: Hitung total dan rata-rata
-angka= int (input ("Input jumlah repeat: "))
+angka = int (input ("Berapa angka?"))
+total = 0
+for i in range (angka):
+    nilai = int(input ("Masukkan angka: "))
+    total = total + nilai
+rata_rata = total/angka
+print ("Total:", total)
+print ("Rata-rata:", rata_rata)
+
+#Challenge 5: Password Attempt (Loop + Break)
+password_benar = "python123"
+for i in range (3):
+    password = input ("Masukkan password: ")
+    if password == password_benar: 
+        print ("Login Berhasil")
+        break
+    else:
+        print ("Password Salah")
+else: 
+    print ("Akses Diblokir")
+
+#Challenge 6: Tebak Angka (Mini Game)
+angka_rahasia = 7
+tebakan = 0
+while tebakan != angka_rahasia:
+    tebakan = int(input("Tebak angka: "))
+    if tebakan < angka_rahasia:
+        print("Terlalu kecil")
+    elif tebakan > angka_rahasia:
+        print("Terlalu besar")
+print("Benar!")
+
+#Challenge 7: Angka Harus Positif
+while True:
+    angka = int(input("Input sini: "))
+    if angka < 0:
+        print("Angka tidak boleh negatif")
+        continue
+    if angka == 0:
+        print("Program berhenti")
+        break
+    print("Valid")
